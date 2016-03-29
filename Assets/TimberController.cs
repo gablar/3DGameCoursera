beee19d971b2937a9680743bf7c54f8727f56934
Gabriel <g.larriuz@gmail.com>
2016-03-29 14:51:18 -0400

    los troncos funcionan
    Los troncos salen disparados de la sierra y caen cerca del  horno.

---
+++ Assets/TimberController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimberController : MonoBehaviour {
+    public float selfDestructTime;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, selfDestructTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
