7552a83be271cb78e0517bc6e47d949f2b3ee0aa
Gabriel <g.larriuz@gmail.com>
2016-04-10 09:50:51 -0400

    Revert "I want to delete this commit"
    This reverts commit 4dae61f36c9f63cfbc1f8de878d075276a356958.

--- Assets/TreeController.cs
+++ Assets/TreeController.cs
@@ -5,22 +5,16 @@ using System;
 public class TreeController : MonoBehaviour {
 
     Animator anim;
-    Rigidbody rgb;
 
-    public bool broken;
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
         anim = GetComponent<Animator>();
-        rgb = GetComponent<Rigidbody>();
         anim.speed = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (anim.GetCurrentAnimatorStateInfo(0).IsName("GrownTree") && broken) {
-            rgb.AddTorque(-transform.forward*20);
-            broken = false; 
-        }
+        
 	}
 
     void OnEnable()
@@ -47,14 +41,5 @@ public class TreeController : MonoBehaviour {
     private void RainStarted()
     {
         anim.speed = 1;
-    }
-
-    void OnCollisionEnter(Collision other) {
-        //Debug.Log("Collision Name:" + other.transform.name);
-        if (other.gameObject.tag == "Cart") {
-            Debug.Log("Collision Tag: Cart ");
-            gameObject.transform.SetParent(other.transform);
-            
-        }
     }
 }
