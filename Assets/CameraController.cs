b66ebea0a1a880868225139cc6d55434a8ad9a4d
Gabriel <g.larriuz@gmail.com>
2016-04-19 12:24:22 -0400

    Added look up and down using scroll wheel

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    public float scrollMultiplier = 1;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.right * Input.GetAxis("Mouse ScrollWheel")*scrollMultiplier);
+    }
+}
