c4b356dfe14dc80bf458c8fb4f23f6c9a10d9b84
Gabriel <g.larriuz@gmail.com>
2016-04-19 14:15:55 -0400

    La musica cambia y se redujo el tamaño

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -11,6 +11,6 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.right * Input.GetAxis("Mouse ScrollWheel")*scrollMultiplier);
+        transform.Rotate(Vector3.right * Input.GetAxis("Mouse ScrollWheel")*scrollMultiplier*Time.deltaTime);
     }
 }
---
+++ Assets/MusicManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class MusicManager : MonoBehaviour {
+    public AudioClip LoopOnClip;
+    public AudioClip LoopOffClip;
+    AudioSource source;
+	// Use this for initialization
+	void Start () {
+	    source = GetComponent<AudioSource>();
+        source.clip = LoopOffClip;
+        source.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void EarthquakeEvent() {
+        source.clip = LoopOffClip;
+    }
+
+
+    void OnEnable()
+    {
+        TerrainController.OnEarthquake += EarthquakeEvent;
+        CloudController.OnRainStarted += RainStarted;
+        CloudController.OnRainStopped += RainStopped;
+    }
+
+
+    void OnDisable()
+    {
+        TerrainController.OnEarthquake -= EarthquakeEvent;
+        CloudController.OnRainStarted -= RainStarted;
+        CloudController.OnRainStopped += RainStopped;
+    }
+
+    private void RainStopped()
+    {
+        source.clip = LoopOffClip;
+        source.Play();
+    }
+
+    private void RainStarted()
+    {
+        source.clip = LoopOnClip;
+        source.Play();
+    }
+}
