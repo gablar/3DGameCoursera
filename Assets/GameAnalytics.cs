c69cc9f1c04e665e6dd05ea63cdceff3ade292db
Gabriel <g.larriuz@gmail.com>
2016-04-21 16:45:48 -0400

    added analytics
    Game started, time of rain started and time of saw started

---
+++ Assets/GameAnalytics.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Analytics;
+using System.Collections.Generic;
+
+public class GameAnalytics : MonoBehaviour {
+    // Use this for initialization
+    bool start;
+	void Start () {
+        start = true;
+       Analytics.CustomEvent("gameStarted", new Dictionary<string, object>
+                              {
+                                { "GameStarted", start }
+                              });
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
